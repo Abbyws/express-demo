40515d66c78c063ff995ba83ae10e8fa9ec855c2
Abbyws <1395878149@qq.com>
2018-06-06 17:48:42 +0800

    change package

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express()
 
 app.get('/', function (req, res) {
-  res.send('王硕 3117370084  hahah')
+  res.send('王硕 3117370084  试试看阿~~')
 })
 
 app.listen(8000, function () {
